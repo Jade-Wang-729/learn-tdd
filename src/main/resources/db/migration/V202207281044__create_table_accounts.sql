24875cd4c4ec9e44ffd9ad601e0b05a728115d70
Jade <yujue_official@163.com>
2022-08-16 16:27:16 +0800

    [config] database: add and config a mysql database

--- src/main/resources/db/migration/V202207281044__create_table_accounts.sql
+++ src/main/resources/db/migration/V202207281044__create_table_accounts.sql
@@ -1,4 +1,4 @@
-create table if not exists ACCOUNTS(
+create table if not exists accounts(
     id varchar(64) primary key,
     username varchar(64) not null unique,
     password varchar(128) not null
