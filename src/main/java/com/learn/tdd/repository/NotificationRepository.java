f0990c13cab6432846b1efe978727080bfbba320
Jade <yujue_official@163.com>
2022-08-21 15:46:21 +0800

    [story-5] Repository: should update entuty when there given existing id and content, and fail when not.

--- src/main/java/com/learn/tdd/repository/NotificationRepository.java
+++ src/main/java/com/learn/tdd/repository/NotificationRepository.java
@@ -3,7 +3,10 @@ package com.learn.tdd.repository;
 import com.learn.tdd.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, String> {
+    Optional<Notification> findById(String id);
+    Optional<Notification> findByIdAndContent(String id, String content);
 }
--- src/test/java/com/learn/tdd/repository/NotificationRepositoryTest.java
+++ src/test/java/com/learn/tdd/repository/NotificationRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class NotificationRepositoryTest extends BaseRepositoryTest {
     @Autowired
@@ -12,12 +16,45 @@ class NotificationRepositoryTest extends BaseRepositoryTest {
 
     @Test
     @Sql("classpath:sql/insertUserToDb.sql")
-    void should_save_success_when_given_an_existing_username_and_valid_content() {
+    void should_save_success_when_given_an_existing_id_and_valid_content() {
         // given
         Notification notification = new Notification("id", "valid content", "unread");
         // when
         notificationRepository.save(notification);
         // then
         dbAssertThat("select * from notifications where id='id'").hasNumberOfRows(1);
+    }
+
+    @Test
+    @Sql("classpath:sql/insertNotificationToDb.sql")
+    void should_update_success_when_given_an_existing_id_and_valid_content() {
+        Optional<Notification> notification = notificationRepository.findByIdAndContent("id","valid content");
+
+        assertThat(notification.isPresent()).isTrue();
+        assertThat(notification.get().getContent()).isEqualTo("valid content");
+
+        notification.get().setStatus("approve");
+        notificationRepository.save(notification.get());
+
+        Optional<Notification> notificationChanged = notificationRepository.findByIdAndContent("id","valid content");
+        notificationChanged.ifPresent(value -> assertThat(value.getStatus()).isEqualTo("approve"));
+
+    }
+
+    @Test
+    @Sql("classpath:sql/insertNotificationToDb.sql")
+    void should_update_fail_when_given_not_existing_id_and_invalid_content() {
+        Optional<Notification> notification = notificationRepository.findByIdAndContent("idWrong","valid content");
+
+        assertThat(notification.isEmpty()).isTrue();
+
+    }
+    @Test
+    @Sql("classpath:sql/insertNotificationToDb.sql")
+    void should_update_fail_when_given_an_existing_id_and_invalid_content() {
+        Optional<Notification> notification = notificationRepository.findByIdAndContent("idWrong","invalid content");
+
+        assertThat(notification.isEmpty()).isTrue();
+
     }
 }
